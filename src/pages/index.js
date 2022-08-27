ecf82f58776ccfc3fef66660eee5f9a00dd1db1d
Dan Wu <danielwu426@gmail.com>
2022-08-27 13:37:12 -0500

    update opengraph image

--- src/pages/index.js
+++ src/pages/index.js
@@ -76,7 +76,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="The on-chain permissions layer for DAOs">
       <HomepageHeader />
       <main>
