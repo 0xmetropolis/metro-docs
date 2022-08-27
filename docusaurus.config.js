5ce270abe30a3b9380fbd72946ee7927adf72ee7
Dan Wu <danielwu426@gmail.com>
2022-08-27 12:27:54 -0500

    Update OpenGraphLogo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/orcaprotocol/orca-docs/blob/main/',
         },
         blog: {
           routeBasePath: 'changelog',
@@ -47,7 +47,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/orcaprotocol/orca-docs/blob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
