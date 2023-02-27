149ab5934c507b5fbd03b93062abc3f292fe2b90
Chun Poon <hello@chunpoon.xyz>
2023-02-27 11:49:14 -0800

    normal casing for doc subtopics

--- src/components/content/GettingStartedCardsContent.js
+++ src/components/content/GettingStartedCardsContent.js
@@ -2,19 +2,19 @@ export const gettingStartedContent = [
     {
       docNumber: '01',
       docTitle: 'Learn Pod Basics →',
-      docSubtopic: 'LEARN',
+      docSubtopic: 'Learn',
       docLink: '/docs/pod-basics/pod-basics',
     },
     {
       docNumber: '02',
       docTitle: 'Create and Manage Pods →',
-      docSubtopic: 'CREATE AND MANAGE',
+      docSubtopic: 'Create and Manage',
       docLink: '/docs/getting-started-pods/creating-pods',
     },
     {
       docNumber: '03',
       docTitle: 'Integrate With Pods →',
-      docSubtopic: 'INTEGRATE',
+      docSubtopic: 'Integrate',
       docLink: '/docs/sdk/sdk-overview',
     },
   ]
